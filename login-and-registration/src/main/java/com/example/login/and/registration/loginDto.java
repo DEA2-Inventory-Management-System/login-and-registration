032a404c436830bf83dc252b76c1f791a0617ef2
Dunuke Meegoda <73906227+dunukem@users.noreply.github.com>
2023-05-14 15:21:07 +0530

    Revert "Add columns"

--- login-and-registration/src/main/java/com/example/login/and/registration/loginDto.java
+++ login-and-registration/src/main/java/com/example/login/and/registration/loginDto.java
@@ -1,23 +1,4 @@
 package com.example.login.and.registration;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import org.springframework.format.annotation.DateTimeFormat;
-
 public class loginDto {
-
-    @Id
-    @Column
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-
-    @Column(name="user_name")
-    private String itemCode;
-
-    @Column(name="password")
-    private String itemName;
-
-
 }
