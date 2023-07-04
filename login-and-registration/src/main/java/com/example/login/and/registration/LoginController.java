19de2b8702dc66a2a0580bef9445276921acb115
Dunuke Meegoda <dunukem123@gmail.com>
2023-07-04 11:44:03 +0530

    Edited LoginContoller

--- login-and-registration/src/main/java/com/example/login/and/registration/LoginController.java
+++ login-and-registration/src/main/java/com/example/login/and/registration/LoginController.java
@@ -1,4 +1,22 @@
 package com.example.login.and.registration;
 
-public class loginController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+
+    @GetMapping("/getUser")
+    private List<LoginDto> getUser() { return LoginService.getUser(); }
+    @PostMapping("/registerUser")
+    private LoginDto saveUser(@RequestBody LoginDto loginDto){
+        LoginService.saveUser(loginDto);
+        return loginDto;
+    }
 }
