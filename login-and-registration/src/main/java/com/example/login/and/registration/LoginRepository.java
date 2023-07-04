6dc8331f23a80822cb26801511579af31934d0c1
Dunuke Meegoda <dunukem123@gmail.com>
2023-07-04 23:42:03 +0530

    LoginRepository

--- login-and-registration/src/main/java/com/example/login/and/registration/LoginRepository.java
+++ login-and-registration/src/main/java/com/example/login/and/registration/LoginRepository.java
@@ -1,4 +1,17 @@
 package com.example.login.and.registration;
 
-public interface loginRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LoginRepository extends JpaRepository<LoginDto,Integer>, JpaSpecificationExecutor<LoginDto> {
+    List<LoginDto> findAll();
+
+    Object save(LoginDto loginDto);
+
+    Optional<LoginDto> findById(int id);
+
+    void deleteById(int id);
 }
